0ca046a343d9fa46797c2c52c9259eb09f2f39f1
Davit Putra Hartono <davit.putra.ft21@mail.umy.ac.id>
2023-12-28 17:49:25 +0700

    Update KontakServis

--- app/src/main/java/com/example/pratikum12/MainActivity.kt
+++ app/src/main/java/com/example/pratikum12/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.pratikum12.ui.ui.KontakApp
 import com.example.pratikum12.ui.ui.Pratikum12Theme
 
 class MainActivity : ComponentActivity() {
@@ -22,25 +23,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    KontakApp()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Pratikum12Theme {
-        Greeting("Android")
     }
 }
