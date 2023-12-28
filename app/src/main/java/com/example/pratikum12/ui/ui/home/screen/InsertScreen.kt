8eee272419c789487d225632fbb5f5fcea09ed6f
Davit Putra Hartono <davit.putra.ft21@mail.umy.ac.id>
2023-12-29 02:10:54 +0700

    add fungsi entry kontak body

--- app/src/main/java/com/example/pratikum12/ui/ui/home/screen/InsertScreen.kt
+++ app/src/main/java/com/example/pratikum12/ui/ui/home/screen/InsertScreen.kt
@@ -1,19 +1,27 @@
 package com.example.pratikum12.ui.ui.home.screen
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.pratikum12.navigation.DestinasiNavigasi
 import com.example.pratikum12.ui.ui.TopAppBarKontak
+import com.example.pratikum12.ui.ui.home.viewmodel.InsertUiEvent
+import com.example.pratikum12.ui.ui.home.viewmodel.InsertUiState
 import com.example.pratikum12.ui.ui.home.viewmodel.InsertViewModel
 import com.example.pratikum12.ui.ui.theme.PenyediaViewModel
 import kotlinx.coroutines.launch
@@ -62,3 +70,28 @@ fun EntryKontakScreen(
 }
 
 
+@Composable
+fun EntryKontakBody(
+    insertUiState: InsertUiState,
+    onSiswaValueChange: (InsertUiEvent) -> Unit,
+    onSaveClick: () -> Unit,
+    modifier: Modifier = Modifier
+){
+    Column (
+        verticalArrangement = Arrangement.spacedBy(18.dp),
+        modifier = modifier.padding(12.dp)
+    ){
+        FormInputSiswa(
+            insertUiEvent = insertUiState.insertUiEvent,
+            onValueChange = onSiswaValueChange,
+            modifier = Modifier.fillMaxWidth()
+        )
+        Button(
+            onClick = onSaveClick,
+            shape = MaterialTheme.shapes.small,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(text = "Simpan")
+        }
+    }
+}
